92257b19f2b0d2f47a554462d3107a0a4dca8eff
Dean McPhee <deanjamesmcphee@gmail.com>
2026-05-09 16:22:31 +1000

    feat: add multi-select for airports with color-coded icons and labels

--- src/components/FlightMap.tsx
+++ src/components/FlightMap.tsx
@@ -13,9 +13,20 @@ const INITIAL_VIEW_STATE = {
   zoom: 2,
 }
 
+const AIRPORT_ICON_COLORS = {
+  BLACK: [0, 0, 0, 215],
+  GREEN: [39, 114, 29, 235],
+} as const;
+
+const AIRPORT_LABEL_COLORS = {
+  BLACK: [0, 0, 0, 215],
+  GREEN: [39, 114, 29, 235],
+} as const;
+
 export default function FlightMap() {
   const [airports, setAirports] = useState<FeaturedAirport[]>([])
   const [hoveredAirportId, setHoveredAirportId] = useState<string | null>(null)
+  const [selectedAirportIds, setSelectedAirportIds] = useState<Set<string>>(new Set())
 
   useEffect(() => {
     loadFeaturedAirports().then(setAirports)
@@ -26,22 +37,33 @@ export default function FlightMap() {
       id: 'airports-icon',
       data: airports,
       getPosition: d => [d.lon, d.lat],
-      getIcon: () => ({ url: '/dot.png', width: 128, height: 128, mask: true }),
+      getIcon: () => ({ url: '/dot2.png', width: 128, height: 128, mask: true }),
       getSize: d => d.iata === hoveredAirportId ? 32 : 24,
       getAngle: d => d.rotation,
-      getColor: [0, 0, 0, 255],
+      getColor: d => selectedAirportIds.has(d.iata) ? AIRPORT_ICON_COLORS.GREEN : AIRPORT_ICON_COLORS.BLACK,
       pickable: true,
       onClick: (info) => {
-        if (info.object) console.log(info.object)
+        if (info.object) {
+          setSelectedAirportIds(prev => {
+            const next = new Set(prev)
+            const id = info.object.iata
+            if (next.has(id)) next.delete(id)
+            else next.add(id)
+            console.log(next)
+            return next
+          })
+        }
       },
       onHover: (info) => {
         setHoveredAirportId(info.object ? info.object.iata : null);
       },
       updateTriggers: {
         getSize: hoveredAirportId,
+        getColor: [...selectedAirportIds].sort().join(','),
       },
       transitions: {
         getSize: { duration: 150, easing: easeCubic },
+        getColor: { duration: 200, easing: easeCubic },
       }
     }),
     new TextLayer<FeaturedAirport>({
@@ -50,20 +72,22 @@ export default function FlightMap() {
       getPosition: d => [d.lon, d.lat],
       getText: d => d.iata,
       getSize: d => d.iata === hoveredAirportId ? 26 : 22,
-      getColor: [0, 0, 0, 200],
+      getColor: d => selectedAirportIds.has(d.iata) ? AIRPORT_LABEL_COLORS.GREEN : AIRPORT_LABEL_COLORS.BLACK,
       getPixelOffset: d => d.iata === hoveredAirportId ? [0, -30] : [0, -26],
       fontFamily: 'Caveat Brush',
       fontWeight: 'normal',
       onHover: (info) => {
-        setHoveredAirportId(info.object ? info.object.id : null);
+        setHoveredAirportId(info.object ? info.object.iata : null);
       },
       updateTriggers: {
         getSize: hoveredAirportId,
         getPixelOffset: hoveredAirportId,
+        getColor: [...selectedAirportIds].sort().join(','),
       },
       transitions: {
         getSize: { duration: 150, easing: easeCubic },
         getPixelOffset: { duration: 150, easing: easeCubic },
+        getColor: { duration: 200, easing: easeCubic },
       }
     }),
   ]
